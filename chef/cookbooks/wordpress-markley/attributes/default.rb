0699daf102935c23e5b06685c3f911c310f63c66
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-15 16:49:38 -0700

    rolling back to mysql 6.

--- chef/cookbooks/wordpress-markley/attributes/default.rb
+++ chef/cookbooks/wordpress-markley/attributes/default.rb
@@ -1,10 +1,10 @@
-default['wordpress-markley']['wp_user'] = node['cloud']['wordpress_user']
-default['wordpress-markley']['wp_password'] = node['cloud']['wordpress_password']
+default['wordpress-markley']['WPAdmin'] = node['cloud']['wp_user']
+default['wordpress-markley']['WPAdminPass'] = node['cloud']['wp_password']
 default['wordpress-markley']['private_key'] = 'kmarkley-chef.pem'
 default['wordpress-markley']['validator_key'] = 'kmarkley-validator.pem'
 default['wordpress-markley']['short_org'] = 'kmarkley'
-default['wordpress-markley']['server'] = node['cloud']['wordpress_server']
-default['wordpress-markley']['email'] = node['cloud']['wordpress_email']
+default['wordpress-markley']['server'] = node['cloud']['WPServer']
+default['wordpress-markley']['email'] = node['cloud']['WPAdminEmail']
 
 # Attributes to override
 override['wordpress']['db']['mysql_version'] = '5.5'
@@ -15,7 +15,6 @@ override['wordpress']['db']['user'] = node['cloud']['db_user']
 override['wordpress']['db']['pass'] = node['cloud']['db_password']
 override['wordpress']['db']['prefix'] = 'wp_'
 override['wordpress']['db']['host'] = 'localhost'
-
 
 override['php']['packages'] = %w(php55 php55-devel php55-cli php-pear)
 override['php']['mysql']['package'] = 'php55-mysqlnd'
