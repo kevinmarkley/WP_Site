9eaad6b1087fab1c614ef0afb8b08c050e919146
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-14 20:05:18 -0700

    trying with out variable

--- chef/cookbooks/chef-server-markley/recipes/default.rb
+++ chef/cookbooks/chef-server-markley/recipes/default.rb
@@ -72,11 +72,227 @@ end
 end
 
 # Upload cookbooks
-%w{chef_handler windows 7-zip build-essential chef-sugar xml yum yum-epel iis yum-mysql-community rbac smf mysql php openssl apache2 apt postgresql database mariadb mysql2_chef_gem tar compat_resource rsyslog bluepill ohai packagecloud runit nginx php-fpm selinux wordpress wordpress-markley}.each do |cookbook|
-  execute "knife cookbook upload #{cookbook}" do
+# %w{chef_handler windows 7-zip build-essential chef-sugar xml yum yum-epel iis yum-mysql-community rbac smf mysql php openssl apache2 apt postgresql database mariadb mysql2_chef_gem tar compat_resource rsyslog bluepill ohai packagecloud runit nginx php-fpm selinux wordpress wordpress-markley}.each do |cookbook|
+#   execute "knife cookbook upload #{cookbook}" do
+#     cwd '/cookbooks'
+#     user 'root'
+#     group 'root'
+#     action :run
+#   end
+
+execute "knife cookbook upload wordpress-markley" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload runit" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload nginx" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload php-fpm" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload selinux" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload wordpress" do
     cwd '/cookbooks'
     user 'root'
     group 'root'
     action :run
   end
+
+execute "knife cookbook upload packagecloud" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload ohai" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload bluepill" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload compat_resource" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload tar" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload mysql2_chef_gem" do
+    cwd '/cookbooks'
+    user 'root'
+    group 'root'
+    action :run
+  end
+
+execute "knife cookbook upload database" do
+    cwd '/cookbooks'
+    user 'root'
+    group 'root'
+    action :run
+  end
+
+execute "knife cookbook upload postgresql" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload apt" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload apache2" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload openssl" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload php" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload mysql" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload smf" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload rbac" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload yum-mysql-community" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload iis" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload yum-epel" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload yum" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload xml" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload chef-sugar" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload build-essential" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload 7-zip" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload windows" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
+end
+
+execute "knife cookbook upload chef_handler" do
+  cwd '/cookbooks'
+  user 'root'
+  group 'root'
+  action :run
 end
