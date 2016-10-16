bc557ca66561ea696632b7b431e1970e4a26bd19
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-16 15:15:35 -0700

    upgrading to php56

--- chef/cookbooks/drupal-markley/attributes/default.rb
+++ chef/cookbooks/drupal-markley/attributes/default.rb
@@ -7,21 +7,17 @@ default['wordpress-markley']['server'] = node['cloud']['WPServer']
 default['wordpress-markley']['email'] = node['cloud']['WPAdminEmail']
 
 # Attributes to override
-override['wordpress']['version'] = 'latest'
-override['wordpress']['dir'] = '/var/www/wordpress/'
 override['wordpress']['db']['mysql_version'] = '5.5'
 override['wordpress']['db']['root_password'] = 'myrootpassword'
 override['wordpress']['db']['instance_name'] = 'default'
 override['wordpress']['db']['name'] = "wordpressdb"
 override['wordpress']['db']['user'] = node['cloud']['db_user']
-override['wordpress']['db']['pass'] = node['cloud']['mysql']['db_password']
+override['wordpress']['db']['pass'] = node['cloud']['db_password']
 override['wordpress']['db']['prefix'] = 'wp_'
 override['wordpress']['db']['host'] = 'localhost'
-override['php']['mysql']['package'] = 'php56-mysqlnd'
-# override['php']['packages'] = %w(php55 php55-devel php55-cli php-pear)
-# override['php']['mysql']['package'] = 'php55-mysqlnd'
+
+override['php']['packages'] = %w(php55 php55-devel php55-cli php-pear)
+override['php']['mysql']['package'] = 'php55-mysqlnd'
 
 override['wordpress']['server_name'] = node['fqdn']
 override['wordpress']['server_aliases'] = [node['fqdn'], "wordpress"]
-
-
--- chef/cookbooks/mysql2_chef_gem/CHANGELOG.md
+++ chef/cookbooks/mysql2_chef_gem/CHANGELOG.md
@@ -1,6 +1,16 @@
-crossplat CHANGELOG
+mysql2_chef_gem CHANGELOG
 ========================
-This file is used to list changes made in each version of the crossplat cookbook.
+
+1.0.1 (2014-12-25)
+------------------
+- Moving from recipe_eval in to include_recipe LWRP
+
+1.0.0 (2014-12-23)
+------------------
+- Replacing recipes with resources
+- Mysql and MariaDB providers for linking mysql2 gem
+- Expanded platform test coverage
+
 0.1.1 (2014-09-15)
 ------------------
 - Correct a typo in documentation
