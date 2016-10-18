bd38e774a12f9948ffe834e0786209b8bf361aeb
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-18 11:59:39 -0700

    Full mysql cookbook

--- chef/cookbooks/mysql2_chef_gem/metadata.rb
+++ chef/cookbooks/mysql2_chef_gem/metadata.rb
@@ -3,8 +3,8 @@ maintainer 'Nicolas Blanc'
 maintainer_email 'sinfomicien@gmail.com'
 license 'Apache 2.0'
 description 'Provides the mysql2_chef_gem resource'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.0'
+
+version '1.0.1'
 
 supports 'amazon'
 supports 'redhat'
@@ -13,11 +13,7 @@ supports 'scientific'
 supports 'fedora'
 supports 'debian'
 supports 'ubuntu'
-supports 'oracle'
 
 depends 'build-essential'
-depends 'mysql', '>= 6.0'
+depends 'mysql'
 depends 'mariadb'
-
-source_url 'https://github.com/sinfomicien/mysql2_chef_gem' if respond_to?(:source_url)
-issues_url 'https://github.com/sinfomicien/mysql2_chef_gem/issues' if respond_to?(:issues_url)
