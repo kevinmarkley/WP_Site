d2b1ebdb52ead2bf97ea47ba1660e8c1212d4790
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-25 13:29:29 -0700

    Wordpress 2.3.0

--- chef/cookbooks/mysql/metadata.rb
+++ chef/cookbooks/mysql/metadata.rb
@@ -3,22 +3,20 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Provides mysql_service, mysql_config, and mysql_client resources'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '6.1.3'
+version '8.0.4'
 
 supports 'amazon'
 supports 'redhat'
 supports 'centos'
 supports 'scientific'
+supports 'oracle'
 supports 'fedora'
 supports 'debian'
 supports 'ubuntu'
-supports 'smartos'
-supports 'omnios'
 supports 'suse'
+supports 'opensuse'
+supports 'opensuseleap'
 
-depends 'yum-mysql-community'
-depends 'smf'
-
-source_url 'https://github.com/chef-cookbooks/mysql' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/mysql/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/mysql'
+issues_url 'https://github.com/chef-cookbooks/mysql/issues'
+chef_version '>= 12.1' if respond_to?(:chef_version)
