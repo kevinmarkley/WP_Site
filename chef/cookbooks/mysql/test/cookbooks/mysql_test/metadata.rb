2092c25617208498dd5c00a5972f2b00711f981d
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-25 13:09:13 -0700

    Wordpress 2.3.0

--- chef/cookbooks/mysql/test/cookbooks/mysql_test/metadata.rb
+++
@@ -1,5 +0,0 @@
-name 'mysql_test'
-version '0.0.1'
-
-depends 'mysql'
-depends 'yum-mysql-community'
--- chef/cookbooks/mysql/test/cookbooks/mysql_test/recipes/installation_client.rb
+++
@@ -1,4 +0,0 @@
-mysql_client_installation_package 'default' do
-  version node['mysql']['version']
-  action :create
-end
