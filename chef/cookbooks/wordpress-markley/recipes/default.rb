1cf716c61f58625db7b1689b004d09da4b7f9d8d
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-26 12:37:22 -0700

    Wordpress 2.3.0

--- chef/cookbooks/wordpress-markley/recipes/default.rb
+++ chef/cookbooks/wordpress-markley/recipes/default.rb
@@ -3,15 +3,15 @@
 
 
 
-yum_repository 'mysql-community-client-5.5.53-2' do
-  mirrorlist 'http://repo.mysql.com/yum/mysql-5.5-community/el/6/x86_64/'
-  description ''
-  enabled true
-  gpgcheck true
-end
+# yum_repository 'mysql-community-client-5.5.53-2' do
+#   mirrorlist 'http://repo.mysql.com/yum/mysql-5.5-community/el/6/x86_64/'
+#   description ''
+#   enabled true
+#   gpgcheck true
+# end
 
 package 'mysql-devel'
-# package 'php55-devel'
+package 'php55-devel'
 
 # include_recipe "wordpress"
 # include_recipe "yum-mysql-community::mysql55"
