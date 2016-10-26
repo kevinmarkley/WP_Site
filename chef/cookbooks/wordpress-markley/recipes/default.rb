0a257162e09da47939e2c6cb996f293bcb0f181b
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-26 10:59:49 -0700

    Wordpress 2.3.0

--- chef/cookbooks/wordpress-markley/recipes/default.rb
+++ chef/cookbooks/wordpress-markley/recipes/default.rb
@@ -1,8 +1,8 @@
 ## See http://wp-cli.org/docs/installing/ for more info
 
-include_recipe "wordpress"
-include_recipe "yum-mysql-community::mysql55"
-package 'mysql-devel'
+# include_recipe "wordpress"
+# include_recipe "yum-mysql-community::mysql55"
+
 
 yum_repository 'mysql55-community' do
   mirrorlist 'http://repo.mysql.com/yum/mysql-5.5-community/el/6/x86_64/'
@@ -11,34 +11,36 @@ yum_repository 'mysql55-community' do
   gpgcheck true
 end
 
+# package 'mysql-devel'
+
 # Download the wp-cli
-execute "download wp-cli" do
-  command "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
-  user 'root'
-  group 'root'
-  action :run
-end
+# execute "download wp-cli" do
+#   command "curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+#   user 'root'
+#   group 'root'
+#   action :run
+# end
 
 # Make the tool executable
-execute "make wp-cli executable" do
-  command "chmod +x wp-cli.phar"
-  user 'root'
-  group 'root'
-  action :run
-end
+# execute "make wp-cli executable" do
+#   command "chmod +x wp-cli.phar"
+#   user 'root'
+#   group 'root'
+#   action :run
+# end
 
 # Move wp-cli
-execute "move wp-cli" do
-  command "mv wp-cli.phar /usr/local/bin/wp"
-  user 'root'
-  group 'root'
-  action :run
-end
+# execute "move wp-cli" do
+#   command "mv wp-cli.phar /usr/local/bin/wp"
+#   user 'root'
+#   group 'root'
+#   action :run
+# end
 
 # Update wp-admin
-execute "update wp-admin" do
-  command "wp core install --path=\"/var/www/wordpress\" --url=\"#{node['wordpress-markley']['server']}\"  --title=\"Kevin Markley WP\" --admin_user=\"#{node['wordpress-markley']['wp_user']}\" --admin_password=\"#{node['wordpress-markley']['wp_password']}\" --admin_email=\"#{node['wordpress-markley']['email']}\""
-  user 'ec2-user'
-  group 'ec2-user'
-  action :run
-end
+# execute "update wp-admin" do
+#   command "wp core install --path=\"/var/www/wordpress\" --url=\"#{node['wordpress-markley']['server']}\"  --title=\"Kevin Markley WP\" --admin_user=\"#{node['wordpress-markley']['wp_user']}\" --admin_password=\"#{node['wordpress-markley']['wp_password']}\" --admin_email=\"#{node['wordpress-markley']['email']}\""
+#   user 'ec2-user'
+#   group 'ec2-user'
+#   action :run
+# end
