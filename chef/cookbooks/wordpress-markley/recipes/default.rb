cecbdd8aa9ceb068f749a314625565139131c439
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-29 13:06:12 -0700

    Wordpress 2.3.0

--- chef/cookbooks/wordpress-markley/recipes/default.rb
+++ chef/cookbooks/wordpress-markley/recipes/default.rb
@@ -1,16 +1,16 @@
 ## See http://wp-cli.org/docs/installing/ for more info
 
 
-# include_recipe "yum-mysql-community::mysql55"
+include_recipe "yum-mysql-community::mysql56"
 
-yum_repository 'mysql56-community' do
-  mirrorlist 'http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/'
-  description 'MySQL Connectors Community'
-  name 'MySQL 5.6 Community Server'
-  enabled true
-  gpgcheck true
-  # gpgkey 'https://raw.githubusercontent.com/rs-services/equinix-public/master/cookbooks/db_mysql/files/centos/mysql_pubkey.asc'
-end
+# yum_repository 'mysql56-community' do
+#   mirrorlist 'http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/'
+#   description 'MySQL Connectors Community'
+#   name 'MySQL 5.6 Community Server'
+#   enabled true
+#   gpgcheck true
+#   # gpgkey 'https://raw.githubusercontent.com/rs-services/equinix-public/master/cookbooks/db_mysql/files/centos/mysql_pubkey.asc'
+# end
 
 package 'mysql-devel'
 # package 'php55-devel'
