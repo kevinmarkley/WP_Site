5bcb269ba4f94ab273865557436d42fefe4bbd3b
kevin.markley <kevin.markley@kibocommerce.com>
2016-10-31 09:22:41 -0700

    Wordpress 2.3.0

--- chef/cookbooks/mysql2_chef_gem/test/fixtures/cookbooks/mysql2_chef_gem_test/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-# comments!
-
-mysql2_chef_gem 'default' do
-  client_version node['mysql2_chef_gem']['client_version'] if node['mysql2_chef_gem']
-  provider Chef::Provider::Mysql2ChefGem::Mysql if node['mysql2_chef_gem']['provider'] == 'mysql'
-  provider Chef::Provider::Mysql2ChefGem::Mariadb if node['mysql2_chef_gem']['provider'] == 'mariadb'
-  action :install
-end
